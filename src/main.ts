73e17b4c422a689dd92caca5aec91ad3122979f5
stasya00 <0.0stasya@gmail.com>
2023-04-16 15:54:30 +0200

    remove description from index html

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { Section } from "./section";
 import { Canvas } from "./canvas";
 import { urlManager } from "./urlManager";
 
 let c = new Canvas();
+throw new Error();
 console.log(urlManager.getCurrentURL());
 c.make();
