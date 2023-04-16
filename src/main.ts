a38d3500d7aed2e7dc7ad2f1acf4ba4ca72ab109
stasya00 <0.0stasya@gmail.com>
2023-04-16 13:34:28 +0200

    fix page manager

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { Section } from "./section";
 import { Canvas } from "./canvas";
+import { urlManager } from "./urlManager";
 
 let c = new Canvas();
+console.log(urlManager.getCurrentURL());
 c.make();
