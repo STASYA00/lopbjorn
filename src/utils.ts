d452a4b707a933077dd094d8f7c0bea4151d7b1e
stasya00 <0.0stasya@gmail.com>
2023-04-16 21:42:29 +0200

    test emoji

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 //import { EmojiConvertor } from "emoji-js";
-import * as EmojiConvertor from 'emoji-js/lib/emoji.js';
+import EmojiConvertor from 'emoji-js';
 
 function emoji(content: string): string{
     let em = new EmojiConvertor();
