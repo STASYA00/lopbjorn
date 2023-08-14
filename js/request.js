3f0cfbd730a76ea382e791081933d7de963b7705
STASYA00 <0.0stasya@gmail.com>
2023-08-14 20:13:57 +0200

    add mode cors

--- js/request.js
+++ js/request.js
@@ -94,6 +94,7 @@ class ServerRequest {
                 'Access-Control-Allow-Methods': 'GET, POST',
                 'Access-Control-Allow-Headers': 'Content-Type'
             },
+            mode: 'cors',
         }).then(result => {
             console.log("RESULT", result);
             if (result.status == 200) {
